6550df81916ac79147b93671148b4f776b011e48
Mikey <mike@michaelbridges.co.uk>
2020-05-23 02:33:56 +0100

    0.92.1 Unity Export Complete
    Scale needs fixing, Unity appearing 100x smaller

--- test_op.py
+++ test_op.py
@@ -1,5 +1,6 @@
 import bpy
 import os
+import math
 
 class EXPORT_OT_Operator(bpy.types.Operator):
     bl_idname = "object.exporthelper"
@@ -9,7 +10,8 @@ class EXPORT_OT_Operator(bpy.types.Operator):
     def execute(self, context):
         
         glb_dir = '.\GLB'
-        fbx_dir = '.\FBX' 
+        fbx_dir = '.\FBX'
+        unity_dir = '.\FBX_Unity'  
         obj_dir = '.\OBJ'
         blend_file_path = bpy.data.filepath
         directory = os.path.dirname(blend_file_path)
@@ -23,7 +25,10 @@ class EXPORT_OT_Operator(bpy.types.Operator):
             os.makedirs(directory + fbx_dir)
             
         if os.path.isdir(directory + glb_dir) == False:
-            os.makedirs(directory + glb_dir)   
+            os.makedirs(directory + glb_dir)
+
+        if os.path.isdir(directory + unity_dir) == False:
+            os.makedirs(directory + unity_dir)     
 
         i = 0
         for i in range(len(bpy.context.scene.objects)):
@@ -79,7 +84,7 @@ class EXPORT_OT_Operator(bpy.types.Operator):
             bpy.data.objects[obj_name].location = obj_loc # set object back to it's original location
             bpy.ops.object.select_all(action='DESELECT')   
             i =+ 1
-            
+
         i = 0    
         for i in range(len(bpy.context.scene.objects)):
                 
@@ -166,6 +171,74 @@ class EXPORT_OT_Operator(bpy.types.Operator):
                 use_metadata=True, 
                 axis_forward='-Z', 
                 axis_up='Y')
+            
+            bpy.data.objects[obj_name].location = obj_loc # set object back to it's original location
+            bpy.ops.object.select_all(action='DESELECT')   
+            i =+ 1
+
+        # Unity Specific Export Settings and Code
+        i = 0    
+        for i in range(len(bpy.context.scene.objects)):
+            
+            obj_name = bpy.context.scene.objects[i].name
+            bpy.data.objects[obj_name].select_set(True)
+            target_file_fbx = os.path.join(directory + unity_dir, obj_name + '.fbx')
+
+            # Unity Faff
+            # Prevents, error - need to warn user
+            bpy.ops.object.make_single_user(type='SELECTED_OBJECTS', object=True, obdata=True)
+
+            # Would prefer to set_rotation? if possible.
+            bpy.ops.transform.rotate(value = math.pi, orient_axis='Z', orient_type='GLOBAL')
+            bpy.ops.transform.rotate(value = (math.pi / 2), orient_axis='X', orient_type='GLOBAL')
+            bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
+
+            bpy.ops.transform.rotate(value = (-math.pi / 2), orient_axis='X', orient_type='GLOBAL')
+
+            obj_loc = bpy.data.objects[obj_name].location.copy() # copy location
+            bpy.data.objects[obj_name].location = (0,0,0) # move object to world origin
+            
+            bpy.ops.export_scene.fbx(
+                filepath=target_file_fbx, 
+                check_existing=False, 
+                filter_glob="*.fbx", 
+                use_selection=True, 
+                use_active_collection=False, 
+                global_scale=1, 
+                apply_unit_scale=True, 
+                apply_scale_options='FBX_SCALE_NONE', 
+                bake_space_transform=False, 
+                object_types={'EMPTY', 'CAMERA', 'LIGHT', 'ARMATURE', 'MESH', 'OTHER'}, 
+                use_mesh_modifiers=True, 
+                use_mesh_modifiers_render=True, 
+                mesh_smooth_type='OFF', 
+                use_subsurf=False, 
+                use_mesh_edges=False, 
+                use_tspace=False, 
+                use_custom_props=False, 
+                add_leaf_bones=True, 
+                primary_bone_axis='Y', 
+                secondary_bone_axis='X', 
+                use_armature_deform_only=False, 
+                armature_nodetype='NULL', 
+                bake_anim=True, 
+                bake_anim_use_all_bones=True, 
+                bake_anim_use_nla_strips=True, 
+                bake_anim_use_all_actions=True, 
+                bake_anim_force_startend_keying=True, 
+                bake_anim_step=1, 
+                bake_anim_simplify_factor=1, 
+                path_mode='AUTO', 
+                embed_textures=False, 
+                batch_mode='OFF', 
+                use_batch_own_dir=True, 
+                use_metadata=True, 
+                axis_forward='-Z', 
+                axis_up='Y')
+
+            ## More Unity Faff, putting objects back to original state for user.
+            bpy.ops.transform.rotate(value = math.pi, orient_axis='Z', orient_type='GLOBAL')
+            bpy.ops.object.transform_apply(location=False, rotation=True, scale=True)
             
             bpy.data.objects[obj_name].location = obj_loc # set object back to it's original location
             bpy.ops.object.select_all(action='DESELECT')   
